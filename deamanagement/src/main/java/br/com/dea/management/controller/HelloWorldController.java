6d48ef1aca62b6fae4dbcaf6cf3646e8472f8802
Felipe Semissatto <fsemissato@apple.com>
2023-01-13 14:40:06 -0300

    BONUS: adding index endpoint and unit tests for both methods

--- deamanagement/src/main/java/br/com/dea/management/controller/HelloWorldController.java
+++ deamanagement/src/main/java/br/com/dea/management/controller/HelloWorldController.java
@@ -1,12 +1,19 @@
 package br.com.dea.management.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Index page!";
+    }
+
     @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
     public ResponseEntity<String> getHelloWorld() {
         return ResponseEntity.ok("Hello, World!");
