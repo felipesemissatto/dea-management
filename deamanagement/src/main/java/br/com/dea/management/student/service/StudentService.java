c7b38c2228a68c0fcac0265e2e944d4a5b01d3b2
Felipe Semissatto <fsemissato@apple.com>
2023-01-26 11:41:57 -0300

    Adding controller and dto for Student

--- deamanagement/src/main/java/br/com/dea/management/student/service/StudentService.java
+++ deamanagement/src/main/java/br/com/dea/management/student/service/StudentService.java
@@ -5,9 +5,11 @@ import br.com.dea.management.student.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class StudentService {
 
     @Autowired
