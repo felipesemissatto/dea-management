6afee3238c526bb8c69124f4ac0a340d888e9571
Felipe Semissatto <fsemissato@apple.com>
2023-02-07 16:54:36 -0300

    Updating controller and service class

--- deamanagement/src/main/java/br/com/dea/management/student/service/StudentService.java
+++ deamanagement/src/main/java/br/com/dea/management/student/service/StudentService.java
@@ -1,10 +1,12 @@
 package br.com.dea.management.student.service;
 
+import br.com.dea.management.exceptions.NotFoundException;
 import br.com.dea.management.student.domain.Student;
 import br.com.dea.management.student.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,10 +23,10 @@ public class StudentService {
     }
 
     public Page<Student> findAllStudentsPaginated(Integer page, Integer pageSize) {
-        return this.studentRepository.findAllPaginated(PageRequest.of(page, pageSize));
+        return this.studentRepository.findAllPaginated(PageRequest.of(page, pageSize, Sort.by("user.name").ascending()));
     }
 
-    public Optional<Student> findStudentById(Long id) {
-        return this.studentRepository.findStudentByUserId(id);
+    public Student findStudentById(Long id) {
+        return this.studentRepository.findById(id).orElseThrow(() -> new NotFoundException(Student.class, id));
     }
 }
