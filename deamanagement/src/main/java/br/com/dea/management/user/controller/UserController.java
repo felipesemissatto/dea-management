44a576979e706c5d58e98eb04284ab48e62f96c7
Felipe Semissatto <fsemissato@apple.com>
2023-02-14 10:14:07 -0300

    Adding Swagger annotations for each endpoint

--- deamanagement/src/main/java/br/com/dea/management/user/controller/UserController.java
+++ deamanagement/src/main/java/br/com/dea/management/user/controller/UserController.java
@@ -2,6 +2,9 @@ package br.com.dea.management.user.controller;
 
 import br.com.dea.management.user.domain.User;
 import br.com.dea.management.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +17,15 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Operation(summary = "Load the list of user paginated.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Page or Page Size params not valid"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
     @GetMapping("/user")
-    public Page<User> getUsers(@RequestParam Integer page,
-                               @RequestParam Integer pageSize) {
+    public Page<User> getUsers(@RequestParam(required = true) Integer page,
+                               @RequestParam(required = true) Integer pageSize) {
 
         Page<User> usersPaged = this.userService.findAllUsersPaginated(page, pageSize);
         return usersPaged;
