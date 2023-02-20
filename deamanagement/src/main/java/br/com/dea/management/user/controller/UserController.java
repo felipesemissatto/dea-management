85c10f1c38a71db60dd70536bcf3cf8219c922b0
Felipe Semissatto <fsemissato@apple.com>
2023-02-20 10:26:30 -0300

    Adding end2end tests for get user by id method

--- deamanagement/src/main/java/br/com/dea/management/user/controller/UserController.java
+++ deamanagement/src/main/java/br/com/dea/management/user/controller/UserController.java
@@ -1,17 +1,21 @@
 package br.com.dea.management.user.controller;
 
+import br.com.dea.management.student.dto.StudentDto;
 import br.com.dea.management.user.domain.User;
 import br.com.dea.management.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -29,5 +33,25 @@ public class UserController {
 
         Page<User> usersPaged = this.userService.findAllUsersPaginated(page, pageSize);
         return usersPaged;
+    }
+
+    @Operation(summary = "Load the user by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "User Id invalid"),
+            @ApiResponse(responseCode = "404", description = "User Not found"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Long id) {
+
+        log.info(String.format("Fetching user by id : Id : %s", id));
+
+        User user = this.userService.findUserById(id);
+
+        log.info(String.format("User loaded successfully : Student : %s", user));
+
+        return user;
+
     }
 }
