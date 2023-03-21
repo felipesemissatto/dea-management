8bef17341d41608abb24bda30d82da6e7254f8a5
Felipe Semissatto <fsemissato@apple.com>
2023-03-21 16:25:13 -0300

    Adding employee DTOs

--- deamanagement/src/main/java/br/com/dea/management/employee/service/EmployeeService.java
+++ deamanagement/src/main/java/br/com/dea/management/employee/service/EmployeeService.java
@@ -1,8 +1,16 @@
 package br.com.dea.management.employee.service;
 
 import br.com.dea.management.employee.domain.Employee;
+import br.com.dea.management.employee.dto.CreateEmployeeRequestDto;
+import br.com.dea.management.employee.dto.UpdateEmployeeRequestDto;
 import br.com.dea.management.employee.repository.EmployeeRepository;
 import br.com.dea.management.exceptions.NotFoundException;
+import br.com.dea.management.position.domain.Position;
+import br.com.dea.management.position.repository.PositionRepository;
+import br.com.dea.management.student.domain.Student;
+import br.com.dea.management.student.dto.CreateStudentRequestDto;
+import br.com.dea.management.student.dto.UpdateStudentRequestDto;
+import br.com.dea.management.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,7 +24,9 @@ public class EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
-
+    @Autowired
+    private PositionRepository positionRepository;
+    
     public List<Employee> findAllEmployees() {
         return this.employeeRepository.findAll();
     }
@@ -27,6 +37,46 @@ public class EmployeeService {
 
     public Employee findEmployeeById(Long id) {
         return this.employeeRepository.findById(id).orElseThrow(() -> new NotFoundException(Employee.class, id));
+    }
+
+    public Employee createEmployee(CreateEmployeeRequestDto createEmployeeRequestDto) {
+        Position position = this.positionRepository.findById(createEmployeeRequestDto.getPosition())
+                .orElseThrow(() -> new NotFoundException(Position.class, createEmployeeRequestDto.getPosition()));
+
+        User user = User.builder()
+                .name(createEmployeeRequestDto.getName())
+                .email(createEmployeeRequestDto.getEmail())
+                .password(createEmployeeRequestDto.getPassword())
+                .linkedin(createEmployeeRequestDto.getLinkedin())
+                .build();
+
+        Employee employee = Employee.builder()
+                .user(user)
+                .employeeType(createEmployeeRequestDto.getEmployeeType())
+                .position(position)
+                .build();
+
+        return this.employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long studentId, UpdateEmployeeRequestDto updateEmployeeRequestDto) {
+
+        Employee employee = this.findEmployeeById(studentId);
+        Position position = this.positionRepository.findById(updateEmployeeRequestDto.getPosition())
+                .orElseThrow(() -> new NotFoundException(Position.class, updateEmployeeRequestDto.getPosition()));
+
+        User user = employee.getUser();
+
+        user.setName(updateEmployeeRequestDto.getName());
+        user.setEmail(updateEmployeeRequestDto.getEmail());
+        user.setPassword(updateEmployeeRequestDto.getPassword());
+        user.setLinkedin(updateEmployeeRequestDto.getLinkedin());
+
+        employee.setUser(user);
+        employee.setEmployeeType(updateEmployeeRequestDto.getEmployeeType());
+        employee.setPosition(position);
+
+        return this.employeeRepository.save(employee);
     }
 
     public void deleteEmployee(Long employeeId) {
