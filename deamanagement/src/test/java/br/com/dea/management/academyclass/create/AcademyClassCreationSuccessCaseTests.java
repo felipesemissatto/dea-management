c2913184b3f7190c49b2a7dea5f4e226e7525f90
Felipe Semissatto <fsemissato@apple.com>
2023-03-28 16:38:22 -0300

    Added automated tests to create endpoint

---
+++ deamanagement/src/test/java/br/com/dea/management/academyclass/create/AcademyClassCreationSuccessCaseTests.java
@@ -0,0 +1,76 @@
+package br.com.dea.management.academyclass.create;
+
+import br.com.dea.management.academyclass.AcademyTestUtils;
+import br.com.dea.management.academyclass.domain.AcademyClass;
+import br.com.dea.management.academyclass.repository.AcademyClassRepository;
+import br.com.dea.management.employee.EmployeeTestUtils;
+import br.com.dea.management.employee.EmployeeType;
+import br.com.dea.management.employee.domain.Employee;
+import br.com.dea.management.employee.repository.EmployeeRepository;
+import br.com.dea.management.position.domain.Position;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AcademyClassCreationSuccessCaseTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AcademyClassRepository academyClassRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private AcademyTestUtils academyTestUtils;
+    @Autowired
+    private EmployeeTestUtils employeeTestUtils;
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    @Test
+    void whenRequestingEmployeeCreationWithAValidPayload_thenCreateAEmployeeSuccessfully() throws Exception {
+        this.academyClassRepository.deleteAll();
+        this.employeeRepository.deleteAll();
+
+        this.employeeTestUtils.createFakeEmployees(1);
+        Employee instructor = this.employeeRepository.findAll().get(0);
+
+        String payload = "{" +
+                "\"startDate\": \"2023-02-27\"," +
+                "\"endDate\": \"2024-02-27\"," +
+                "\"instructorId\": " + instructor.getId() +
+                "}";
+        mockMvc.perform(post("/academy-class")
+                        .contentType(APPLICATION_JSON_UTF8).content(payload))
+                .andExpect(status().isOk());
+
+        AcademyClass academyClass = this.academyClassRepository.findAll().get(0);
+
+        assertThat(academyClass.getStartDate()).isEqualTo("2023-02-27");
+        assertThat(academyClass.getEndDate()).isEqualTo("2024-02-27");
+        assertThat(academyClass.getInstructor().getId()).isEqualTo(instructor.getId());
+        assertThat(academyClass.getInstructor().getClass()).isEqualTo(instructor.getClass());
+        assertThat(academyClass.getInstructor().getUser().getId()).isEqualTo(instructor.getUser().getId());
+        assertThat(academyClass.getInstructor().getPosition().getId()).isEqualTo(instructor.getPosition().getId());
+    }
+}
