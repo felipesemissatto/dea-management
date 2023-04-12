cc9f7bbd2657cda3543e3ab6d2e3436bce72c243
Felipe Semissatto <fsemissato@apple.com>
2023-04-12 09:32:28 -0300

    Automated test when requesting project list with valid data created

---
+++ deamanagement/src/test/java/br/com/dea/management/project/get/ProjectGetAllTests.java
@@ -0,0 +1,73 @@
+package br.com.dea.management.project.get;
+
+import br.com.dea.management.employee.repository.EmployeeRepository;
+import br.com.dea.management.project.ProjectTestUtil;
+import br.com.dea.management.project.repository.ProjectRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Slf4j
+public class ProjectGetAllTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ProjectTestUtil projectTestUtil;
+
+    @BeforeEach
+    void beforeEach() {
+        log.info("Before each test in " + ProjectGetAllTests.class.getSimpleName());
+    }
+
+    @BeforeAll
+    void beforeSuiteTest() {
+        log.info("Before all tests in " + ProjectGetAllTests.class.getSimpleName());
+    }
+
+    @Test
+    void whenRequestingProjectList_thenReturnListOfProjectPaginatedSuccessfully() throws Exception {
+        this.projectRepository.deleteAll();
+        this.employeeRepository.deleteAll();
+
+        this.projectTestUtil.createFakeProject(1);
+
+        this.mockMvc.perform(get("/project?page=0&pageSize=4"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].name", is("name 0")))
+                .andExpect(jsonPath("$.content[0].client", is("client 0")))
+                .andExpect(jsonPath("$.content[0].startDate", is("2023-04-12")))
+                .andExpect(jsonPath("$.content[0].endDate", is("2024-04-12")))
+                .andExpect(jsonPath("$.content[0].externalProductManager", is("external product manager 0")))
+                .andExpect(jsonPath("$.content[0].productOwner.name", is("name 0")))
+                .andExpect(jsonPath("$.content[0].scrumMaster.name", is("name 1")));
+    }
+}
